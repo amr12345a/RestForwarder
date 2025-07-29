94c7c7f42be0237d3a87d3b49819c7eb9c01c224
Amr Elkhamisy <31047545+amr12345a@users.noreply.github.com>
2025-07-29 17:04:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/items/:id', (req, res) => {
 
 // POST endpoint to create a new item
 app.post('/items', (req, res) => {
-  const newItem = id: req.body;
+  const newItem = req.body;
   items.push(newItem);
   res.status(201).json(newItem);
 });
